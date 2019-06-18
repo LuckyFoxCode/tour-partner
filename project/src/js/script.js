30f7f966cf749b14c06920a7adddfe90d859fd34
vikkidanylevska <dva13072016@gmail.com>
2019-06-18 16:47:54 +0300

    add team-section

---
+++ project/src/js/script.js
@@ -0,0 +1,23 @@
+ $(".slider-team").owlCarousel({
+    items: 4,
+    margin: 30,
+    // autoplay: true,
+    autoplaySpeed: 600,
+    autoplayTimeout : 3000,
+    autoplayHoverPause: true,
+    loop: true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:2,
+        },
+        992:{
+            items:3,
+        },
+        1200:{
+            items:4,
+        },
+    }
+});
