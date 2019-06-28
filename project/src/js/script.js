31fa7f10531dcdd1d71cce270decd63e8ca76409
Serhii <sergei181989@gmail.com>
2019-06-29 00:23:17 +0300

    TourPartner: fixed bug on header.ejs

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -2,25 +2,13 @@
 (function () {
 	'use strict'
 
-	$(document).ready(function () {
-
-		// Fixed header__top
-		const headerTop = $('.header__top');
-
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 0) {
-				headerTop.addClass('fixed');
-			} else {
-				headerTop.removeClass('fixed');
-			}
-		});
-
-		// add/remove active class in .header__top-nav-item
-		$('.header__top-nav-item').click(function() {
-			$('.header__top-nav-item').removeClass('active');
-			$(this).addClass('active');
-		});
-
-	});
+	// Fixed header__top
+	window.onscroll = function () {
+		if (window.pageYOffset >= 1) {
+			$(".header__top").addClass("fixed__top");
+		} else {
+			$(".header__top").removeClass("fixed__top");
+		}
+	}
 
 })();
