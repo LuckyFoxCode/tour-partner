7708c6db1adc6616137cd1bb422e9ad9b7f578d8
HlebBekesevich <bigdogkrok@gmail.com>
2019-06-29 10:46:52 +0300

    fixed bug

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -1,4 +1,21 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1) $('head').addClass("scroll_head");
-    else $('head').removeClass("scroll_head");
+$(document).ready(function() {
+	if ($(".menu_bar").css("display") == "none") {
+		$(".head__items_link").show();
+	}
+	$(".menu_bar").click(function() {
+		if ($(".head__items_link").css("display") == "none") {
+			$(".head__items_link").show();
+		}
+		else {
+			$(".head__items_link").hide();
+		}
+	});
 });
+
+window.onscroll = () => {
+	if (window.pageYOffset >= 1){
+		$(".header").addClass("scroll_head");
+	} else {
+		$(".header").removeClass("scroll_head");
+	}
+}
