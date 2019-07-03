86ed855b1562856435d484ca554f081591885fc2
vikkidanylevska <dva13072016@gmail.com>
2019-07-03 13:01:24 +0300

    links added and changed map

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -25,14 +25,13 @@ let marker;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 49.0649202, lng: 33.4101731},
+    center: {lat: 49.0642379, lng: 33.4048909},
     zoom: 15,
     disableDefaultUI: true
   }),
 
   marker = new google.maps.Marker({
-      position: {lat: 49.0666141, lng: 33.4136125},
-      map: map,
-      // animation: google.maps.Animation.BOUNCE,
+      position: {lat: 49.0642379, lng: 33.4048909},
+      map: map
   });
 }
